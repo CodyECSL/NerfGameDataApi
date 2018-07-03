560dd5bf38256c2f2f0577b9718bfe1c93eeb2a4
Cody C <ccoane@gmail.com>
2018-07-02 20:18:03 -0500

    Include express-list-endpoints module to display API Endpoint list at base URL

--- Server.js
+++ Server.js
@@ -2,8 +2,12 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var routerClass = require('./router');
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+var listEndpoints = require("express-list-endpoints");
+var apiList = null;
 
 
 router.use(function (req,res,next) {
@@ -13,6 +17,13 @@ router.use(function (req,res,next) {
 
 app.use("/",router);
 app.use("/", routerClass);
+
+app.get("/", function (req, res) {
+  console.info(apiList);
+  res.send(apiList);
+});
+
+apiList = listEndpoints(app);
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
