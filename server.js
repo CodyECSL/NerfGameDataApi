ce4bedf8e55ac180ecef9d711837f54c306103d5
Cody C <ccoane@gmail.com>
2018-10-29 21:52:40 -0500

    Blanket enable CORS

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var kothRoute = require("./kothRoute")
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var listEndpoints = require("express-list-endpoints");
 var apiList = null;
